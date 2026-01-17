92d4c5d1d270517e24e1ba8d65f7abfd8cb7a1fd
Dana Gabbay <dana.gabbay@post.runi.ac.il>
2026-01-17 13:15:32 +0200

    second

--- List.java
+++ List.java
@@ -24,26 +24,49 @@ public class List {
 
     /** Returns the CharData of the first element in this list. */
     public CharData getFirst() {
-        // Your code goes here
         return first.cp;
     }
 
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
-        // Your code goes here
+        CharData x = new CharData(chr);
+        Node y = new Node(x);
+        y.next=first;
+        first = y;
+        size++;
+
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
-        // Your code goes here
-        return "";
+        String result= "";
+        Node pointer = first;
+        while (pointer!=null)
+        {
+            result = result + pointer;
+            if (pointer.next!=null){
+                result= result+ " ";
+            }
+            pointer=pointer.next;
+        }
+            
+        return result;
     }
 
     /** Returns the index of the first CharData object in this list
      *  that has the same chr value as the given char,
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
-        // Your code goes here
+        Node equal = first;
+        int i=0;
+        while (equal!=null)
+        {
+            if (equal.cp.equals(chr))
+                return i;
+            equal = equal.next;
+            i++;
+        }
+
         return -1;
     }
 
@@ -51,14 +74,39 @@ public class List {
      *  increments its counter. Otherwise, adds a new CharData object with the
      *  given chr to the beginning of this list. */
     public void update(char chr) {
-        // Your code goes here
+        Node pointer = first;
+        while (pointer!=null)
+        {
+            if (pointer.cp.equals(chr)){
+                pointer.cp.count++;
+                return;
+            }
+            pointer=pointer.next;   
+        }
+        addFirst(chr);
     }
 
     /** GIVE If the given character exists in one of the CharData objects
      *  in this list, removes this CharData object from the list and returns
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
-        // Your code goes here
+        Node current = first;
+        Node prev = null;
+        while (current!=null){
+            if (current.cp.chr == chr){
+                if (prev==null){
+                    first = current.next;
+                }
+                else {
+                    prev.next = current.next;
+                }
+                size--;
+                return true;
+            }
+            prev = current;
+            current = current.next;
+
+        }
         return false;
     }
 
@@ -66,8 +114,14 @@ public class List {
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
-        // Your code goes here
-        return null;
+        if (index<0 || index>=size)
+            return null;
+        Node pointer = first;
+        for (int i=0; i<index; i++)
+        {
+            pointer = pointer.next;
+        }
+        return pointer.cp;
     }
 
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
